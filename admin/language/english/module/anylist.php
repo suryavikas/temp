5492d691e758e4d743f9636ed0508f03ae199224
Surya Vikas <suryavikas@aol.com>
2013-03-03 18:22:21 +0530

    Added languge file for any list

---
+++ admin/language/english/module/anylist.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']       = 'AnyList: List of any products - anywhere';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module AnyList!';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_select_all']     = 'Select all';
+$_['text_unselect_all']   = 'Unselect all';
+$_['text_view_grid']      = 'Grid';
+$_['text_view_list']      = 'List';
+$_['text_period']         = 'Only in period';
+
+// Entry
+$_['entry_selection']     = 'Selection criteria';
+$_['entry_latest_text']   = 'Last x added products ("Latest")';
+$_['entry_specials_text'] = 'First x "Specials" products';
+$_['entry_product']       = 'Product <span style="display: inline;" class="help">(Autocomplete)</span>:';
+$_['entry_category']      = 'Category:';
+$_['entry_products']      = 'Products:';
+$_['entry_manufacturer']  = 'Manufacturers:';
+$_['entry_limit_help']    = 'If no items are selected AnyList always will be shown. If select any item, only when any of this items are shown AnyList will be visible.';
+
+$_['entry_limit']         = 'Limit products to:';
+$_['entry_title']         = 'Box title:';
+$_['entry_titlelink']     = 'Link of box title:';
+$_['entry_code']          = 'Code of box <span style="display: inline;" class="help">(for import tasks)</span>:';
+$_['entry_dimension']     = 'Image (W x H):';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort Order:';
+$_['entry_sort_descending']='sort descending';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module AnyList!';
+$_['error_dimension']     = 'Width &amp; Height dimensions required!';
+?>
